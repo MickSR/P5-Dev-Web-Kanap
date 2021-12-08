dcc8391dbe059982450bdcfbe944d1e775236eb5
MickSR <91461892+MickSR@users.noreply.github.com>
2021-12-08 13:04:15 +0100

    commit sur master

---
+++ front/js/product.js
@@ -0,0 +1,17 @@
+const image = document.getElementsByClassName('item__img');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+const colors = document.getElementById('colors');
+
+let imageURL = "";
+let imageAlt = "";
+
+fetch("http://localhost:3000/api/products/")
+    .then(res => res.json())
+    .then(product => {
+        image[0]
+
+    })
+    
+
---
+++ front/js/script.js
@@ -0,0 +1,34 @@
+    //récupération de l'api
+    fetch("http://localhost:3000/api/products/") 
+    .then(res => res.json())
+    .then(res => Products(res))
+
+    //message en cas d'erreur serveur
+      .catch((error) => { 
+        console.log("Erreur de la requête API");
+      })
+
+    function Products(res) {
+
+    // "product" => élément "res" => itérable
+        for (product of res) { 
+        console.log("Product")
+
+        //on cherche dans le html les éléments à integrer avec l'ID
+        //.innerHTML +=`` le "+" pour ajouter le contenus tant qu'il y en a .
+        //on récupère dans le html à partir de la ligne 52 qu'on remplace par une chaîne de caractère (texte dans une variable)
+            const item = document.getElementById('items').innerHTML +=` 
+            <a href="./product.html?id=${product._id}">
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+            </article>
+            </a>
+      `;
+    }
+}
+
+
+
+ 
