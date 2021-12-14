310249dc7bf0475e505132bddb03f58803b80a5b
MickSR <91461892+MickSR@users.noreply.github.com>
2021-12-14 08:58:46 +0100

    commit sur master

--- front/js/product.js
+++ front/js/product.js
@@ -29,4 +29,4 @@ fetch("http://localhost:3000/api/products/" + New)
         console.log("Erreur de la requête API");
     })
  
-//mettre en place l'ajout panier avec les variable
+//mettre en place l'ajout panier
